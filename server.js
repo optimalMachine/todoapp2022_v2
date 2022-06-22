2098c256c128e5ce2a80e6266a4b9e7aa1c0688b
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-06-21 20:05:50 -0500

    several updates
    completed POST features

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
 app.listen(3000, ()=>{
     console.log('listening on ' + 3000);
 })
@@ -11,4 +13,11 @@ app.get("/",(req,res)=>{
 
 app.get("/write",(req,res)=>{
     res.sendFile(__dirname+"/write.html");
+})
+
+app.post("/add",(req,res)=>{
+    console.log(req.body.title);
+    console.log(req.body.date);
+    res.send("전송완료");
+
 })
